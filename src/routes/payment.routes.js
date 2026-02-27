b439a67a67626fc5440eea5675de42af9e78c934
devindirathuge <devindi.20232705@iit.ac.lk>
2026-02-27 17:46:48 +0530

    Create payment.routes.js
    add API routes for creating and confirming payment intents

---
+++ src/routes/payment.routes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const paymentController = require('../controllers/payment.controller');
+
+router.post('/create-intent', paymentController.createPaymentIntent);
+router.post('/confirm', paymentController.confirmPayment);
+
+module.exports = router;
