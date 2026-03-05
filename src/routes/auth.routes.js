d058f7c51a1073e3e0e1dba4307cedfad7b0c6cf
Nicol Fernando <166232317+sharindanic@users.noreply.github.com>
2026-03-05 10:53:37 +0530

    Update auth.routes.js

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,10 +1,15 @@
+// src/routes/auth.routes.js
 const router = require("express").Router();
-const authController = require("../controllers/auth.controller");
+const authMiddleware = require("../middleware/auth");
+const controller = require("../controllers/auth.controller");
 
 // POST /api/auth/register
-router.post("/register", authController.register);
+router.post("/register", controller.register);
 
 // POST /api/auth/login
-router.post("/login", authController.login);
+router.post("/login", controller.login);
+
+// PUT /api/auth/select-user-type
+router.put("/select-user-type", authMiddleware, controller.selectUserType);
 
 module.exports = router;
