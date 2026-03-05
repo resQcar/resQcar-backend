aacb45075d978b7a3e692e5d3d11309e6a0783fb
Nicol Fernando <166232317+sharindanic@users.noreply.github.com>
2026-03-05 14:03:10 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,14 +1,13 @@
 // src/server.js
-require("dotenv").config();
 const http = require("http");
-const { app } = require("./app");
+const app = require("./app");
 const { initWebSocket } = require("./websocket/socket");
 
 const server = http.createServer(app);
-const wsHub = initWebSocket(server);
 
-// so controllers can use wsHub
+// Initialize WebSocket
+const wsHub = initWebSocket(server);
 app.locals.wsHub = wsHub;
 
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`API running on :${PORT}`));
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
