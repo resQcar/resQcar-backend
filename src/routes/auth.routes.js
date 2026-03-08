0f345ede27401726b6aab22343e28edd2fcd4a0e
shevon16 <shevontharaka1234@gmail.com>
2026-03-08 11:31:42 +0530

    Update auth.routes.js

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,15 +1,9 @@
-// src/routes/auth.routes.js
 const router = require("express").Router();
-const authMiddleware = require("../middleware/auth");
+const { requireAuth } = require("../middleware/auth");
 const controller = require("../controllers/auth.controller");
 
-// POST /api/auth/register
 router.post("/register", controller.register);
-
-// POST /api/auth/login
 router.post("/login", controller.login);
-
-// PUT /api/auth/select-user-type
-router.put("/select-user-type", authMiddleware, controller.selectUserType);
+router.put("/select-user-type", requireAuth, controller.selectUserType);
 
 module.exports = router;
