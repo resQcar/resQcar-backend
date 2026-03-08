449665f9eeefb9242bf0e838bfd2a3a9be778e8e
shevon16 <shevontharaka1234@gmail.com>
2026-03-08 10:01:08 +0530

    update job routes.js

--- src/routes/jobs.routes.js
+++ src/routes/jobs.routes.js
@@ -1,6 +1,13 @@
 const router = require("express").Router();
 const { acceptOffer } = require("../controllers/jobs.controller");
+const { requireAuth } = require("../middleware/auth");
+const jobsController = require("../controllers/jobs.controller");
 
+// Imanjith's route
 router.post("/offers/:offerId/accept", acceptOffer);
+
+// Shevon's routes
+// PUT /api/jobs/:id/status (Update Job Status En Route)
+router.put("/:id/status", requireAuth, jobsController.updateJobStatus);
 
 module.exports = router;
