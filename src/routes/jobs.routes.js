c41e0bdba55674b4daccf731a1ea303eaaa438e2
shevon16 <shevontharaka1234@gmail.com>
2026-03-08 11:20:33 +0530

    update jobs.routes.js

--- src/routes/jobs.routes.js
+++ src/routes/jobs.routes.js
@@ -1,6 +1,10 @@
 const router = require("express").Router();
-const { acceptOffer } = require("../controllers/jobs.controller");
+const { requireAuth } = require("../middleware/auth");
+const jobsController = require("../controllers/jobs.controller");
 
-router.post("/offers/:offerId/accept", acceptOffer);
+router.post("/offers/:offerId/accept", jobsController.acceptOffer);
+router.put("/:id/status", requireAuth, jobsController.updateJobStatus);
+router.put("/:id/complete", requireAuth, jobsController.completeJob);
+router.post("/:id/additional-work", requireAuth, jobsController.requestAdditionalWork);
 
 module.exports = router;
