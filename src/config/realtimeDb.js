05f527449e7512099eda1f10af86960e5c833477
Nicol Fernando <166232317+sharindanic@users.noreply.github.com>
2026-03-11 16:07:54 +0530

    Update realtimeDb.js

--- src/config/realtimeDb.js
+++ src/config/realtimeDb.js
@@ -1,7 +1,7 @@
+// src/config/realtimeDb.js
 const admin = require('firebase-admin');
 
 // Reference to Firebase Realtime Database
 // Used for live location tracking (real-time updates)
 const realtimeDb = admin.database();
-
 module.exports = realtimeDb;
