1c83381bde5b8114f683059f928feb79fb22b2ae
sharindanic <nicolfernando515@gmail.com>
2026-04-07 15:44:57 +0530

    bug fix

--- src/routes/tow-trucks.routes.js
+++ src/routes/tow-trucks.routes.js
@@ -4,18 +4,18 @@ const router = express.Router();
 const towTruckController = require('../controllers/tow-trucks.controller');
 const { requireAuth } = require('../middleware/auth');
 
-// 🔴 HIGH PRIORITY
+
 router.post('/request', requireAuth, towTruckController.requestTowTruck);
 router.get('/available', requireAuth, towTruckController.getAvailableTowTrucks);
 
-// 🟡 MEDIUM PRIORITY
+
 // NOTE: Specific named routes MUST come before /:id
 router.get('/flatbed', requireAuth, towTruckController.getFlatbedTowTrucks);
 router.get('/hook-chain', requireAuth, towTruckController.getHookChainTowTrucks);
 router.get('/wheel-lift', requireAuth, towTruckController.getWheelLiftTowTrucks);
 router.put('/accept/:id', requireAuth, towTruckController.acceptTowRequest);
 
-// 🟢 LOW PRIORITY
+
 router.get('/:id', requireAuth, towTruckController.getTowTruckProfile);
 
 module.exports = router;
